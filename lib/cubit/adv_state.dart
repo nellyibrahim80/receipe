8cdb6c75e0669d4e383f6067197d11d1da801f9e
nellyibrahim80 <nellyibrahim144@gmail.com>
2024-01-11 16:15:42 +0200

    Task of day 9 using Cubit and converting data of Homepage to data driven from json file

--- lib/cubit/adv_state.dart
+++ lib/cubit/adv_state.dart
@@ -1,6 +1,25 @@
 part of 'adv_cubit.dart';
 
-@immutable
-abstract class AdvState {}
+
+abstract class AdvState {
+ final List<Advs> advList=[];
+
+}
 
 class AdvInitial extends AdvState {}
+
+// adv_state.dart
+
+
+
+class AdvLoaded extends AdvState {
+  final List<Advs> advList;
+
+  AdvLoaded({required this.advList});
+}
+
+class AdvError extends AdvState {
+  final String errorMessage;
+
+  AdvError({required this.errorMessage});
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,9 +3,11 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import '../cubit/adv_cubit.dart';
 import '../models/advs.dart';
 import '../models/recipe.dart';
 import '../services/shared_pref.dart';
@@ -26,8 +28,8 @@ class _HomePageState extends State<HomePage> {
   int current = 0;
   CarouselController carousel = CarouselController();
   TextEditingController searchCtrl = TextEditingController();
-  List<Advs> advList=[];
-  List<Recipes> RecipeList=[];
+  List<Advs> advList = [];
+  List<Recipes> RecipeList = [];
   @override
   void initState() {
     // TODO: implement initState
@@ -38,13 +40,14 @@ class _HomePageState extends State<HomePage> {
 
   void initdata() async {
     var adsData = await rootBundle.loadString("assets/data/data.json");
-    var decodedData = List<Map<String, dynamic>>.from(jsonDecode(adsData)["ads"]);
-    var decodedRecipe=List<Map<String,dynamic>>.from(jsonDecode(adsData)["recipes"]);
-    advList=decodedData.map((e) => Advs.fromJson(e)).toList();
-    RecipeList=decodedRecipe.map((e) => Recipes.fromJson(e)).toList();
+    var decodedData =
+        List<Map<String, dynamic>>.from(jsonDecode(adsData)["ads"]);
+    var decodedRecipe =
+        List<Map<String, dynamic>>.from(jsonDecode(adsData)["recipes"]);
+    advList = decodedData.map((e) => Advs.fromJson(e)).toList();
+    RecipeList = decodedRecipe.map((e) => Recipes.fromJson(e)).toList();
     print("----------------$decodedRecipe");
     print("----------------$RecipeList");
-
   }
 
   @override
@@ -89,10 +92,11 @@ class _HomePageState extends State<HomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-              //    Text("Bonjour, ${SharedPrefClass.pref.getString("Email")}", //With Abstract class
-                    Text("Bonjour, ${GetIt.I.get<SharedPreferences>().getString("Email")}", //With Get It singleton
+                  //    Text("Bonjour, ${SharedPrefClass.pref.getString("Email")}", //With Abstract class
+                  Text(
+                      "Bonjour, ${GetIt.I.get<SharedPreferences>().getString("Email")}", //With Get It singleton
 
-                        style:
+                      style:
                           const TextStyle(color: Color(ConstColors.textInput))),
                 ],
               ),
@@ -113,9 +117,9 @@ class _HomePageState extends State<HomePage> {
                         hintText: "Search",
                         prefixIcon: Icon(Icons.search),
                         prefixIconColor: Color(ConstColors.textInput),
-                        enabledBorder:const OutlineInputBorder(
-                            borderSide:  BorderSide(
-                                color: Color(ConstColors.bgInput))),
+                        enabledBorder: const OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color(ConstColors.bgInput))),
 
                         border: OutlineInputBorder(
                             borderSide: const BorderSide(
@@ -127,7 +131,8 @@ class _HomePageState extends State<HomePage> {
                           fontWeight: FontWeight.normal,
                         ),
                       ),
-                      style: const TextStyle(color: Color(ConstColors.textInput)),
+                      style:
+                          const TextStyle(color: Color(ConstColors.textInput)),
                     ),
                   ),
                   Container(
@@ -147,7 +152,17 @@ class _HomePageState extends State<HomePage> {
               SizedBox(
                 width: 370,
                 height: 200,
-                child: Stack(children: [
+                child: BlocConsumer<AdvCubit, AdvState>(
+  listener: (context, state) {
+    print(state.toString());
+    // TODO: implement listener
+  },
+  builder: (context, state) {
+    print(state);
+    if(state is AdvInitial){
+      return CircularProgressIndicator();
+    }else{
+    return Stack(children: [
                   Column(
                     children: [
                       CarouselSlider(
@@ -165,8 +180,8 @@ class _HomePageState extends State<HomePage> {
                             builder: (BuildContext context) {
                               return Container(
                                   width: MediaQuery.of(context).size.width,
-                                  margin:
-                                      const EdgeInsets.symmetric(horizontal: 5.0),
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 5.0),
                                   decoration: BoxDecoration(
                                       image: DecorationImage(
                                           image: AssetImage(
@@ -199,19 +214,6 @@ class _HomePageState extends State<HomePage> {
                           setState(() {});
                         },
                       ),
-                      // AnimatedSmoothIndicator(
-                      //
-                      //   onDotClicked: (position) async {
-                      //     print("---------------->$position");
-                      //     current = position;
-                      //     await carousel.animateToPage(position);
-                      //     setState(() {});
-                      //   },
-
-                      //   activeIndex: current,
-                      //   count: 5,
-                      //   effect: WormEffect(),
-                      // )
                     ],
                   ),
                   Positioned(
@@ -240,71 +242,81 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
-                ]),
+                ]);}
+  },
+),
               ),
               SeeAllHeader(
                 HeaderTitle: "Today's Fresh Recipes",
               ),
               SizedBox(
                 height: 210,
-                child: ListView.builder(itemBuilder: (context, index) {
-                  Recipes recipe=RecipeList[index];
-                  return  SizedBox(
-                    width: 185,
-                    // height: 100,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadiusDirectional.circular(25)),
-                        color: Color(ConstColors.bgInput),
-                        elevation: 0,
-                        //shadowColor: Colors.white,
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Icon(
-                                  Icons.favorite_border,
-                                  color: Color(ConstColors.textInput),
-                                ),
-
-                                Container(
-                                  width: 90,
-                                  height: 80,
-                                  child: OverflowBox(
-                                    alignment: FractionalOffset.topLeft,
-                                    maxWidth: 130,
-                                    child: Image.asset(
-                                      "assets/images/${recipe.image}",
-                                      width: 200, // Ensure that this value does not exceed maxWidth
-                                      height: 80,
-                                      //fit: BoxFit.cover,
-                                    ),
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    Recipes recipe = RecipeList[index];
+                    return SizedBox(
+                      width: 185,
+                      // height: 100,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadiusDirectional.circular(25)),
+                          color: Color(ConstColors.bgInput),
+                          elevation: 0,
+                          //shadowColor: Colors.white,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Icon(
+                                    Icons.favorite_border,
+                                    color: Color(ConstColors.textInput),
                                   ),
-                                )
-                              ],
-                            ),
-                            RecipeDetails(RecipeIndex: index ,starsPadding: 6, recipeList: RecipeList,)
-                          ],
+                                  Container(
+                                    width: 90,
+                                    height: 80,
+                                    child: OverflowBox(
+                                      alignment: FractionalOffset.topLeft,
+                                      maxWidth: 130,
+                                      child: Image.asset(
+                                        "assets/images/${recipe.image}",
+                                        width:
+                                            200, // Ensure that this value does not exceed maxWidth
+                                        height: 80,
+                                        //fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              RecipeDetails(
+                                RecipeIndex: index,
+                                starsPadding: 6,
+                                recipeList: RecipeList,
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
-                scrollDirection: Axis.horizontal,
+                    );
+                  },
+                  scrollDirection: Axis.horizontal,
                   itemCount: RecipeList.length,
-
                 ),
               ),
-
-
               SeeAllHeader(
                 HeaderTitle: "Recommended",
               ),
-              if(RecipeList!=Null)Recommended(recipeList: RecipeList,)else CircularProgressIndicator(),
+              if (RecipeList != Null)
+                Recommended(
+                  recipeList: RecipeList,
+                )
+              else
+                CircularProgressIndicator(),
             ],
           ),
         ),
